265c0d3da45f4bd3972f7e617841c97cc88e2259
RinoTsuka <rino.tsuka.1226@gmail.com>
2020-03-14 22:19:08 +0900

    menu修正

--- wp-content/themes/sora-maru/functions.php
+++ wp-content/themes/sora-maru/functions.php
@@ -1,3 +1,6 @@
 <?php
 
-register_nav_menu('footer-nav', 'メニュー');
+function register_my_menus() {
+  register_nav_menu('footer-nav', 'フッターナビゲーション');
+}
+add_action('after_setup_theme', 'register_my_menus');
